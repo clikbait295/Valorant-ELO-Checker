c861e65ed0d3d3379095824dd92d08180bf55044
clikbait295 <72671591+clikbait295@users.noreply.github.com>
2020-12-31 17:23:38 -0500

    Add files via upload

---
+++ Valorant-ELO-Checker/main.py
@@ -0,0 +1,224 @@
+import json
+from valApi import ValorantAPI
+import time
+import socket
+import tkinter as tk
+hostname = socket.gethostname()
+from tkinter import *
+from functools import partial
+from PIL import ImageTk, Image
+global matchHistory
+import sys
+match_movement_hash = {
+  'INCREASE': ['Increase', 'Victory'],
+  'MINOR_INCREASE': ['Minor Increase', 'Victory'],
+  'MAJOR_INCREASE': ['Major Increase', 'Victory'],
+  'DECREASE': ['Decrease', 'Defeat'],
+  'MAJOR_DECREASE': ['Major Decrease', 'Defeat'],
+  'MINOR_DECREASE': ['Minor Decrease', 'Defeat'],
+  'PROMOTED': ['Promoted', 'Victory'],
+  'DEMOTED': ['Demoted', 'Defeat'],
+  'STABLE': ['Stable', 'Draw']
+}
+
+# Gives map name
+maps_hash = {
+  '/Game/Maps/Duality/Duality': 'Bind',
+  '/Game/Maps/Bonsai/Bonsai': 'Split',
+  '/Game/Maps/Ascent/Ascent': 'Ascent',
+  '/Game/Maps/Port/Port': 'Icebox',
+  '/Game/Maps/Triad/Triad': 'Haven',
+  '': 'Unknown'
+}
+
+
+def normalMode(usernameText, passwordText):
+        username=usernameText.get()
+        password=passwordText.get()
+        region = "NA"
+        client_ip = socket.gethostbyname(hostname)
+
+        # Attempt login
+        try:
+            valorant = ValorantAPI(username, password, region, client_ip)
+        except:
+            print('A login error occurred. F')
+            print('Invalid username/password or incorrect region.')
+
+        # Attempt to acquire match history data
+        try:
+            json_res = valorant.get_match_history()
+        except:
+            print('Cannot get match history.')
+            
+
+        # Attempt to parse through match history data
+
+        posts = []
+        for match in json_res['Matches']:
+          # print(match)
+          if match['CompetitiveMovement'] == 'MOVEMENT_UNKNOWN':
+            continue
+          match_movement, game_outcome = match_movement_hash[match['CompetitiveMovement']]
+          lp_change = ''
+
+          game_map = maps_hash[match['MapID']]
+
+          tier = match['TierAfterUpdate']
+          
+          epoch_time = match['MatchStartTime'] // 1000
+          date = time.strftime('%m-%d-%Y', time.localtime(epoch_time))
+
+          before = match['TierProgressBeforeUpdate']
+          after = match['TierProgressAfterUpdate']
+
+          # calculate ELO change
+          if match['CompetitiveMovement'] == 'PROMOTED':
+            lp_change = '+' + str(after + 100 - before)
+          elif match['CompetitiveMovement'] == 'DEMOTED':
+            lp_change = '-' + str(before + 100 - after)
+          else:
+            if before < after:
+              # won
+              lp_change = '+' + str(after - before)
+            else:
+              # lost
+              lp_change = str(after - before)
+
+          match_data = {
+            'lp_change': lp_change,
+            'current_lp': after,
+            'game_outcome': game_outcome,
+            'movement': match_movement,
+            'tier': tier,
+            'date': date,
+            'game_map': game_map,        
+          }
+
+          posts.append(match_data)
+
+        #print(posts)
+        matchHistory = tk.Toplevel(tkWindow)
+        matchHistory.iconbitmap('favicon.ico')
+        matchHistory.title('Match History for ' + username)
+        for i, n in enumerate(posts):
+                MyLabel(matchHistory,(posts[i]["game_map"]+", "+posts[i]["date"]), str(posts[i]["current_lp"]), posts[i]["tier"], posts[i]["lp_change"], posts[i]["movement"]).pack(expand=True, fill='x')
+        Button(matchHistory, text="Switch to Overlay Mode", command=lambda:[matchHistory.destroy(), overlayMode(username, password)]).pack()
+        Button(matchHistory, text="Refresh", command=lambda:[matchHistory.destroy(), normalMode()]).pack()
+        return
+
+def overlayMode(username, password):
+        region = "NA"
+        client_ip = socket.gethostbyname(hostname)
+
+        # Attempt login
+        try:
+            valorant = ValorantAPI(username, password, region, client_ip)
+        except:
+            print('A login error occurred. F')
+            print('Invalid username/password or incorrect region.')
+
+        # Attempt to acquire match history data
+        try:
+            json_res = valorant.get_match_history()
+        except:
+            print('Cannot get match history.')
+            
+
+        # Attempt to parse through match history data
+
+        posts = []
+        for match in json_res['Matches']:
+          if match['CompetitiveMovement'] == 'MOVEMENT_UNKNOWN':
+            continue
+          match_movement, game_outcome = match_movement_hash[match['CompetitiveMovement']]
+          lp_change = ''
+
+          game_map = maps_hash[match['MapID']]
+
+          tier = match['TierAfterUpdate']
+          
+          epoch_time = match['MatchStartTime'] // 1000
+          date = time.strftime('%m-%d-%Y', time.localtime(epoch_time))
+
+          before = match['TierProgressBeforeUpdate']
+          after = match['TierProgressAfterUpdate']
+
+          # calculate ELO change
+          if match['CompetitiveMovement'] == 'PROMOTED':
+            lp_change = '+' + str(after + 100 - before)
+          elif match['CompetitiveMovement'] == 'DEMOTED':
+            lp_change = '-' + str(before + 100 - after)
+          else:
+            if before < after:
+              # won
+              lp_change = '+' + str(after - before)
+            else:
+              # lost
+              lp_change = str(after - before)
+
+          match_data = {
+            'lp_change': lp_change,
+            'current_lp': after,
+            'game_outcome': game_outcome,
+            'movement': match_movement,
+            'tier': tier,
+            'date': date,
+            'game_map': game_map,        
+          }
+
+          posts.append(match_data)
+
+        matchHistory = tk.Toplevel(tkWindow)
+        matchHistory.title('Match History for ' + username)
+        matchHistory.iconbitmap('favicon.ico')
+        matchHistory.attributes("-topmost", True)
+        matchHistory.geometry('+0-0')  
+        matchHistory.overrideredirect(1)
+        MyLabel(matchHistory,(posts[0]["game_map"]+", "+posts[0]["date"]), str(posts[0]["current_lp"]), posts[0]["tier"], posts[0]["lp_change"], posts[0]["movement"]).pack(expand=True, fill='x')
+        refresh = lambda _:[matchHistory.destroy(),overlayMode(username,password)]
+        normal = lambda _:[matchHistory.destroy(),normalMode()]
+        quit = lambda _:[tkWindow.destroy(),sys.exit()]
+        matchHistory.bind("<Alt_L><r>", refresh)
+        matchHistory.bind("<Alt_L><n>", normal)
+        matchHistory.bind("<Alt_L><q>", quit)
+        return
+
+class MyLabel(Frame):
+  def __init__(self, master, mapDate, currentELO, currentRank, eloChange, rankChange):
+    Frame.__init__(self, master, bd=2, relief='raised')
+    self.rankPic = ImageTk.PhotoImage(Image.open("ranks/"+str(currentRank)+'.png'))
+    Label(self, image = self.rankPic).grid(row=0, column=0, rowspan=2)
+    Label(self, text=mapDate, font=('Aria', 16, 'bold'), fg='black', anchor='w').grid(row=0, column=1)
+    Label(self, text="Current ELO (out of 100): "+currentELO, font=('Aria', 10), fg='black').grid(row=1, column=1)
+    if int(eloChange)==0:
+            Label(self, text=rankChange+", "+eloChange, fg='white', bg='#848785').grid(row=0, column=2, rowspan=2, padx=10)
+    if int(eloChange)>0:
+            Label(self, text=rankChange+", "+eloChange, fg='white', bg='#3ac75f').grid(row=0, column=2, rowspan=2, padx=10)
+    else:
+            Label(self, text=rankChange+", "+eloChange, fg='white', bg='#bf4747').grid(row=0, column=2, rowspan=2, padx=10)
+    self.columnconfigure(1, weight=1)
+
+
+#window
+tkWindow = Tk()  
+tkWindow.geometry('230x160')  
+tkWindow.title('Login to Riot')
+
+#username label and text entry box
+usernameLabel = Label(tkWindow, text="Username").grid(row=0, column=0)
+usernameText = StringVar()
+usernameEntry = Entry(tkWindow, textvariable=usernameText).grid(row=0, column=1)  
+
+#password label and password entry box
+passwordLabel = Label(tkWindow,text="Password").grid(row=1, column=0, pady=13)  
+passwordText = StringVar()
+passwordEntry = Entry(tkWindow, textvariable=passwordText, show='•').grid(row=1, column=1,pady=13)
+normalMode= partial(normalMode, usernameText, passwordText)
+#login button
+loginButton = Button(tkWindow, text="Login", command=normalMode, height=3).place(x=185,y=0)  
+notice = Label(tkWindow,text="This application is not affiliated with Riot Games, or any of its games. This application simply uses Riot's API to find your ELO. No personal info is gathered or saved by this application.\n~ Created by Arnav Ambre ~", wraplength=230, justify="left").place(x=0,y=60)  
+tkWindow.iconbitmap('favicon.ico')
+tkWindow.mainloop()
+
+
---
+++ Valorant-ELO-Checker/valApi.py
@@ -0,0 +1,94 @@
+import requests
+import json
+import urllib.parse
+
+class ValorantAPI(object):
+  access_token = None
+  cookies = None
+  entitlements_token = None
+
+  def __init__(self, username, password, region, client_ip):
+    self.username = username
+    self.password = password
+    self.region = region
+    self.client_ip = client_ip
+
+    self.cookies = self.get_cookies()
+
+    self.access_token = self.get_access_token()
+
+    self.entitlements_token = self.get_entitlements_token()
+
+    self.user_info, self.game_name = self.get_user_info()
+
+  def get_cookies(self):
+    data = {
+    'client_id': 'play-valorant-web-prod',
+    'nonce': '1',
+    'redirect_uri': 'https://beta.playvalorant.com/opt_in',
+    'response_type': 'token id_token',
+    'scope': 'account openid',
+    }
+    headers = {
+      'X-Forwarded-For': self.client_ip
+    }
+    r = requests.post('https://auth.riotgames.com/api/v1/authorization', headers=headers, json=data)
+
+    cookies = r.cookies
+
+    return cookies
+
+  def get_access_token(self):
+    data = {
+      'type': 'auth',
+      'username': self.username,
+      'password': self.password
+    }
+    headers = {
+      'X-Forwarded-For': self.client_ip
+    }
+    r = requests.put('https://auth.riotgames.com/api/v1/authorization', headers=headers, json=data, cookies=self.cookies)
+    uri = r.json()['response']['parameters']['uri']
+    jsonUri = urllib.parse.parse_qs(uri)
+
+    access_token = jsonUri['https://beta.playvalorant.com/opt_in#access_token'][0]
+    #print("Access Token: "+access_token)
+    return access_token
+
+  def get_entitlements_token(self):
+    headers = {
+      'Authorization': f'Bearer {self.access_token}',
+      'X-Forwarded-For': self.client_ip
+    }
+    r = requests.post('https://entitlements.auth.riotgames.com/api/token/v1', headers=headers, json={}, cookies=self.cookies)
+
+    entitlements_token = r.json()['entitlements_token']
+    #print("\nEntitlement Token: " + entitlements_token)
+    return entitlements_token
+
+  def get_user_info(self):
+    headers = {
+      'Authorization': f'Bearer {self.access_token}',
+      'X-Forwarded-For': self.client_ip
+    }
+
+    r = requests.post('https://auth.riotgames.com/userinfo', headers=headers, json={}, cookies=self.cookies)
+    jsonData = r.json()
+    user_info = jsonData['sub']
+    name = jsonData['acct']['game_name']
+    tag  = jsonData['acct']['tag_line']
+    game_name = name + ' #' +  tag
+
+    return user_info, game_name
+
+  def get_match_history(self):
+    headers = {
+      'Authorization': f'Bearer {self.access_token}',
+      'X-Riot-Entitlements-JWT': f'{self.entitlements_token}',
+      'X-Forwarded-For': self.client_ip
+    }
+    r = requests.get(f'https://pd.{self.region}.a.pvp.net/mmr/v1/players/{self.user_info}/competitiveupdates?startIndex=0&endIndex=20', headers=headers, cookies=self.cookies)
+
+    jsonData = r.json()
+
+    return jsonData
